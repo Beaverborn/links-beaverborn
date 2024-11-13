da13e9d17b10823ba0273f5415a1e8c94ea42313
Josh Ehlinger <ehlien@ehlien.dev>
2024-11-12 23:20:54 -0500

    Add Error Handling - Add handling for missing start-date and end-date attributes - Copy HTML Collection into array so there is no race condition   when removing elements that the collection matches (timed class)

--- js/timedUpdate.js
+++ js/timedUpdate.js
@@ -1,6 +1,10 @@
 function timedUpdate(currentDate = new Date()) {
-  const timedElements = document.getElementsByClassName("timed");
+  const timedElementsLive = document.getElementsByClassName("timed");
+  const timedElements = Array.from(timedElementsLive);
   for(let element of timedElements) {
+    if(! element.hasAttribute("start-date") && element.hasAttribute("end-date")) {
+      continue;
+    }
     let startDate = new Date(element.getAttribute("start-date"));
     let endDate = new Date(element.getAttribute("end-date"))
     let isWithinTime = startDate <= currentDate && currentDate <= endDate;
