d0ba2b9e564c61ec9c7f22379db518b7e7a75c32
Josh Ehlinger <ehlien@ehlien.dev>
2025-01-07 23:18:03 -0500

    Update timedUpdate function to handle current year attribute and change OCC to use it

--- js/timedUpdate.js
+++ js/timedUpdate.js
@@ -1,14 +1,36 @@
 function timedUpdate(currentDate = new Date()) {
+
+  // Get all elements with the class "timed"
+  // and store it in an array so that it doesn't update with the page
   const timedElementsLive = document.getElementsByClassName("timed");
   const timedElements = Array.from(timedElementsLive);
+  
+  // Get year value of the current date
+  const currentYear = currentDate.getFullYear();
+
+  // Iterate over elements to find ones that have the required attributes
+  // and make visible if within the start and end dates
   for(let element of timedElements) {
     if(! element.hasAttribute("start-date") && element.hasAttribute("end-date")) {
       continue;
     }
-    let startDate = new Date(element.getAttribute("start-date"));
-    let endDate = new Date(element.getAttribute("end-date"))
+    // Save these attributes as strings since they not be valid date format yet
+    let startDate = element.getAttribute("start-date");
+    let endDate = element.getAttribute("end-date");
+
+    // Use current year to seed start and end dates if use-current-year attribute is present
+    if(element.hasAttribute("use-current-year")) {
+      startDate = currentYear + "-" + startDate;
+      endDate = currentYear + "-" + endDate;
+    }
+    // Convert into Date objects now that format should be valid
+    startDate = new Date(startDate);
+    endDate = new Date(endDate);
+
+    // Check that current date is within range and make visible if so
     let isWithinTime = startDate <= currentDate && currentDate <= endDate;
     if(isWithinTime) {
+      // The timed class hides the element - remove it to make visible on page
       element.classList.remove("timed");
     }
   };
